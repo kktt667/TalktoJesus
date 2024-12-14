9f9378b2a5be8693d4f5ee772f0a7a5265d18323
amtrehan <keshu.trehan@gmail.com>
2024-12-14 19:06:56 +0000

    icons

--- components/Icons.tsx
+++ components/Icons.tsx
@@ -1,159 +1,138 @@
-// components/Icons.tsx
+// components/icons.tsx
+import React from 'react';
+
 export const ActsIcon = () => (
-    <svg 
-      width="32" 
-      height="32" 
-      viewBox="0 0 32 32" 
-      fill="none" 
-      xmlns="http://www.w3.org/2000/svg"
-      className="text-orange-600"
-    >
-      <path 
-        d="M16 4C17.5 7 19.5 8.5 22 9C19.5 9.5 17.5 11 16 14C14.5 11 12.5 9.5 10 9C12.5 8.5 14.5 7 16 4Z" 
-        fill="currentColor"
-        stroke="currentColor"
-        strokeWidth="1"
-      />
-      <path 
-        d="M16 18C17.5 21 19.5 22.5 22 23C19.5 23.5 17.5 25 16 28C14.5 25 12.5 23.5 10 23C12.5 22.5 14.5 21 16 18Z" 
-        fill="currentColor"
-        stroke="currentColor"
-        strokeWidth="1"
-      />
-    </svg>
-  );
-  
-  export const PrayerIcon = () => (
-    <svg 
-      width="32" 
-      height="32" 
-      viewBox="0 0 32 32" 
-      fill="none" 
-      xmlns="http://www.w3.org/2000/svg"
-      className="text-orange-600"
-    >
-      <path 
-        d="M16 5C14 8 13 12 13 16C13 20 14 24 16 27C18 24 19 20 19 16C19 12 18 8 16 5Z" 
-        fill="currentColor"
-      />
-      <path 
-        d="M12 2C10 5 9 9 9 13C9 17 10 21 12 24C14 21 15 17 15 13C15 9 14 5 12 2Z" 
-        fill="currentColor"
-        opacity="0.7"
-      />
-      <path 
-        d="M20 2C18 5 17 9 17 13C17 17 18 21 20 24C22 21 23 17 23 13C23 9 22 5 20 2Z" 
-        fill="currentColor"
-        opacity="0.7"
-      />
-    </svg>
-  );
-  
-  export const ParableIcon = () => (
-    <svg 
-      width="32" 
-      height="32" 
-      viewBox="0 0 32 32" 
-      fill="none" 
-      xmlns="http://www.w3.org/2000/svg"
-      className="text-orange-600"
-    >
-      <path 
-        d="M6 4H26V28H6V4Z" 
-        stroke="currentColor" 
-        strokeWidth="2"
-        fill="none"
-      />
-      <path 
-        d="M10 8H22M10 16H22M10 24H18" 
-        stroke="currentColor" 
-        strokeWidth="2"
-      />
-    </svg>
-  );
-  
-  export const CrossIcon = () => (
-    <svg 
-      width="32" 
-      height="32" 
-      viewBox="0 0 32 32" 
-      fill="none" 
-      xmlns="http://www.w3.org/2000/svg"
-      className="text-orange-600"
-    >
-      <path 
-        d="M16 4V28M10 16H22" 
-        stroke="currentColor" 
-        strokeWidth="2"
-        strokeLinecap="round"
-      />
-    </svg>
-  );
-  
-  export const DoveIcon = () => (
-    <svg 
-      width="32" 
-      height="32" 
-      viewBox="0 0 32 32" 
-      fill="none" 
-      xmlns="http://www.w3.org/2000/svg"
-      className="text-orange-600"
-    >
-      <path 
-        d="M16 8C12 8 9 11 9 15C9 19 12 22 16 22C20 22 23 19 23 15C23 11 20 8 16 8Z" 
-        fill="currentColor"
-      />
-      <path 
-        d="M16 4L19 7L16 10L13 7L16 4Z" 
-        fill="currentColor"
-      />
-      <path 
-        d="M16 20L19 23L16 26L13 23L16 20Z" 
-        fill="currentColor"
-      />
-    </svg>
-  );
-  
-  export const HeartIcon = () => (
-    <svg 
-      width="32" 
-      height="32" 
-      viewBox="0 0 32 32" 
-      fill="none" 
-      xmlns="http://www.w3.org/2000/svg"
-      className="text-orange-600"
-    >
-      <path 
-        d="M16 26C16 26 24 20 24 14C24 10 21 7 17 7C15 7 16 9 16 9C16 9 17 7 15 7C11 7 8 10 8 14C8 20 16 26 16 26Z" 
-        fill="currentColor"
-      />
-    </svg>
-  );
-  
-  export const ScriptureIcon = () => (
-    <svg 
-      width="32" 
-      height="32" 
-      viewBox="0 0 32 32" 
-      fill="none" 
-      xmlns="http://www.w3.org/2000/svg"
-      className="text-orange-600"
-    >
-      <path 
-        d="M8 4H24V28H8V4Z" 
-        stroke="currentColor" 
-        strokeWidth="2"
-        fill="none"
-      />
-      <path 
-        d="M12 8L20 8M12 14L20 14M12 20L16 20" 
-        stroke="currentColor" 
-        strokeWidth="2"
-      />
-      <path 
-        d="M6 7H8M6 16H8M6 25H8" 
-        stroke="currentColor" 
-        strokeWidth="2"
-      />
-    </svg>
-  );
+  <svg 
+    width="32" 
+    height="32" 
+    viewBox="0 0 32 32" 
+    fill="none" 
+    xmlns="http://www.w3.org/2000/svg"
+    className="text-orange-600"
+  >
+    <path 
+      d="M16 4C17.5 7 19.5 8.5 22 9C19.5 9.5 17.5 11 16 14C14.5 11 12.5 9.5 10 9C12.5 8.5 14.5 7 16 4Z" 
+      fill="currentColor"
+    />
+  </svg>
+);
+
+export const PrayerIcon = () => (
+  <svg 
+    width="32" 
+    height="32" 
+    viewBox="0 0 32 32" 
+    fill="none" 
+    xmlns="http://www.w3.org/2000/svg"
+    className="text-orange-600"
+  >
+    <path 
+      d="M16 5C14 8 13 12 13 16C13 20 14 24 16 27C18 24 19 20 19 16C19 12 18 8 16 5Z" 
+      fill="currentColor"
+    />
+  </svg>
+);
+
+export const ParableIcon = () => (
+  <svg 
+    width="32" 
+    height="32" 
+    viewBox="0 0 32 32" 
+    fill="none" 
+    xmlns="http://www.w3.org/2000/svg"
+    className="text-orange-600"
+  >
+    <path 
+      d="M6 4H26V28H6V4Z" 
+      stroke="currentColor" 
+      strokeWidth="2"
+      fill="none"
+    />
+    <path 
+      d="M10 8H22M10 16H22M10 24H18" 
+      stroke="currentColor" 
+      strokeWidth="2"
+    />
+  </svg>
+);
+
+export const CrossIcon = () => (
+  <svg 
+    width="32" 
+    height="32" 
+    viewBox="0 0 32 32" 
+    fill="none" 
+    xmlns="http://www.w3.org/2000/svg"
+    className="text-orange-600"
+  >
+    <path 
+      d="M16 4V28M10 16H22" 
+      stroke="currentColor" 
+      strokeWidth="2"
+      strokeLinecap="round"
+    />
+  </svg>
+);
+
+export const DoveIcon = () => (
+  <svg 
+    width="32" 
+    height="32" 
+    viewBox="0 0 32 32" 
+    fill="none" 
+    xmlns="http://www.w3.org/2000/svg"
+    className="text-orange-600"
+  >
+    <path 
+      d="M16 8C12 8 9 11 9 15C9 19 12 22 16 22C20 22 23 19 23 15C23 11 20 8 16 8Z" 
+      fill="currentColor"
+    />
+    <path 
+      d="M16 4L19 7L16 10L13 7L16 4Z" 
+      fill="currentColor"
+    />
+    <path 
+      d="M16 20L19 23L16 26L13 23L16 20Z" 
+      fill="currentColor"
+    />
+  </svg>
+);
+
+export const HeartIcon = () => (
+  <svg 
+    width="32" 
+    height="32" 
+    viewBox="0 0 32 32" 
+    fill="none" 
+    xmlns="http://www.w3.org/2000/svg"
+    className="text-orange-600"
+  >
+    <path 
+      d="M16 26C16 26 24 20 24 14C24 10 21 7 17 7C15 7 16 9 16 9C16 9 17 7 15 7C11 7 8 10 8 14C8 20 16 26 16 26Z" 
+      fill="currentColor"
+    />
+  </svg>
+);
+
+export const ScriptureIcon = () => (
+  <svg 
+    width="32" 
+    height="32" 
+    viewBox="0 0 32 32" 
+    fill="none" 
+    xmlns="http://www.w3.org/2000/svg"
+    className="text-orange-600"
+  >
+    <path 
+      d="M8 4H24V28H8V4Z" 
+      stroke="currentColor" 
+      strokeWidth="2"
+      fill="none"
+    />
+    <path 
+      d="M12 8L20 8M12 14L20 14M12 20L16 20" 
+      stroke="currentColor" 
+      strokeWidth="2"
+    />
+  </svg>
+);
